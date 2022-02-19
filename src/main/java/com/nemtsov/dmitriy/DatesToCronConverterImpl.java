44a1a26291ca2db9e0a4702aa7c000654fa8b57e
Dmitrii Nemtsov <dimas224@yandex.ru>
2022-02-19 09:59:08 +0300

    Task done

--- src/main/java/com/nemtsov/dmitriy/DatesToCronConverterImpl.java
+++ src/main/java/com/nemtsov/dmitriy/DatesToCronConverterImpl.java
@@ -3,10 +3,15 @@ package com.nemtsov.dmitriy;
 import com.digdes.school.DatesToCronConvertException;
 import com.digdes.school.DatesToCronConverter;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import org.springframework.scheduling.support.CronExpression;
 
 public class DatesToCronConverterImpl implements DatesToCronConverter {
 
@@ -17,17 +22,151 @@ public class DatesToCronConverterImpl implements DatesToCronConverter {
     }
 
     List<LocalDateTime> dates = new ArrayList<>();
+
     try {
       list.forEach(item -> dates.add(LocalDateTime.parse(item)));
     } catch (DateTimeParseException | NullPointerException e) {
       throw new DatesToCronConvertException();
     }
 
-    return "Converted";
+    if (dates.size() == 1) {
+      return String.join(" ",
+              String.valueOf(dates.get(0).getSecond()),
+              String.valueOf(dates.get(0).getMinute()),
+              String.valueOf(dates.get(0).getHour()),
+              String.valueOf(dates.get(0).getDayOfMonth()),
+              String.valueOf(dates.get(0).getMonthValue()),
+              dates.get(0).getDayOfWeek().toString().substring(0, 3));
+    }
+    return findExpression(dates);
   }
 
   @Override
   public String getImplementationInfo() {
-    return null;
+    return String.join("\n",
+            "Credentials: Nemtsov Dmitriy Sergeevich",
+            "Class: " + this.getClass().getSimpleName(),
+            this.getClass().getPackage().toString(),
+            "GitHub: https://github.com/dsnemtsov/cron-converter");
+  }
+
+  private String findExpression(List<LocalDateTime> dates) throws DatesToCronConvertException {
+
+    Collections.sort(dates);
+
+    Set<Integer> seconds = new TreeSet<>();
+    Set<Integer> minutes = new TreeSet<>();
+    Set<Integer> hours = new TreeSet<>();
+    Set<Integer> daysOfMonth = new TreeSet<>();
+    Set<Integer> months = new TreeSet<>();
+
+    String cronSeconds;
+    String cronMinutes;
+    String cronHours;
+    String cronDaysOfMonth;
+    String cronMonths;
+    String cronDayOfWeek;
+
+    dates.forEach(date -> {
+      seconds.add(date.getSecond());
+      minutes.add(date.getMinute());
+      hours.add(date.getHour());
+      daysOfMonth.add(date.getDayOfMonth());
+      months.add(date.getMonthValue());
+    });
+
+    cronSeconds = findValue(seconds, dates);
+    cronMinutes = findValue(minutes, dates);
+    cronHours = findValue(hours, dates);
+    cronDaysOfMonth = findValue(daysOfMonth, dates);
+    cronMonths = findValue(months, dates);
+    cronDayOfWeek = getDayOfWeek(dates);
+
+    String result = String.join(" ", cronSeconds, cronMinutes, cronHours, cronDaysOfMonth, cronMonths, cronDayOfWeek);
+
+    if (checkResult(result, dates)) {
+
+      return getImprovedResult(result, dates);
+    } else {
+      throw new DatesToCronConvertException();
+    }
+  }
+
+  private String getImprovedResult(String result, List<LocalDateTime> dates) {
+    String improvedResult = result;
+    String[] resultToArray = result.split(" ");
+
+    for (String s : resultToArray) {
+      if (s.contains(",")) {
+        Integer[] numbers = Arrays.stream(s.split(",")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+        Set<Integer> partOfTimes = new TreeSet<>(Arrays.asList(numbers));
+        int period = getPeriod(partOfTimes);
+        if (period != -1) {
+          String possibleResult;
+          if (period == 1) {
+            possibleResult = improvedResult.replace(s, "*");
+          } else {
+            possibleResult = improvedResult.replace(s, "*/" + period);
+          }
+          if (checkResult(possibleResult, dates)) {
+            improvedResult = possibleResult;
+          }
+        }
+      }
+    }
+    return improvedResult;
+  }
+
+  private boolean checkResult(String result, List<LocalDateTime> dates) {
+    CronExpression cronExpression = CronExpression.parse(result);
+    List<LocalDateTime> cronResult = new ArrayList<>();
+
+    cronResult.add(dates.get(0));
+
+    for (int i = 1; i < dates.size(); i++) {
+      cronResult.add(cronExpression.next(cronResult.get(i - 1)));
+    }
+    return cronResult.equals(dates);
+  }
+
+  private String getDayOfWeek(List<LocalDateTime> dates) {
+    for (int i = 1; i < dates.size(); i++) {
+      if (!dates.get(i).getDayOfWeek().equals(dates.get(0).getDayOfWeek())) {
+        return "*";
+      }
+    }
+    return dates.get(0).getDayOfWeek().toString().substring(0, 3);
+  }
+
+  private String findValue(Set<Integer> input, List<LocalDateTime> dates) {
+    String result = "*";
+
+    if (input.size() == 1) {
+      result = input.iterator().next().toString();
+    } else if (input.size() == dates.size()) {
+      int period = getPeriod(input);
+      if (period != -1) {
+        if (period == 1) {
+          result = "*";
+        } else {
+          result = "*/" + period;
+        }
+      }
+    } else {
+      result = input.stream().map(String::valueOf).collect(Collectors.joining(","));
+    }
+    return result;
+  }
+
+  private int getPeriod(Set<Integer> input) {
+    Integer[] inputToArray = input.toArray(new Integer[0]);
+    int period = inputToArray[1] - inputToArray[0];
+
+    for (int i = 2; i < inputToArray.length; i++) {
+      if (inputToArray[i] - inputToArray[i - 1] != period) {
+        return -1;
+      }
+    }
+    return period;
   }
 }
--- src/test/java/DatesToCronConverterImplTest.java
+++ src/test/java/DatesToCronConverterImplTest.java
@@ -30,21 +30,80 @@ class DatesToCronConverterImplTest {
   }
 
   @Test
-  void convert_shouldConvert_whenInputCorrect() throws DatesToCronConvertException {
-    List<String> list = Stream.of(
+  void convert_shouldConvert_whenInputCorrect() {
+    List<String> list1 = Stream.of(
             "2022-01-24T19:53:00",
-            "2022-01-24T19:54:00",
             "2022-01-24T19:55:00",
+            "2022-01-24T19:54:00",
+            "2022-01-24T20:02:00",
             "2022-01-24T19:56:00",
             "2022-01-24T19:57:00",
             "2022-01-24T19:58:00",
             "2022-01-24T19:59:00",
             "2022-01-24T20:00:00",
-            "2022-01-24T20:01:00",
-            "2022-01-24T20:02:00"
+            "2022-01-24T20:01:00"
     ).collect(Collectors.toList());
 
-    assertEquals("Converted", converter.convert(list));
+    List<String> list2 = Stream.of(
+            "2022-01-25T08:00:00",
+            "2022-01-25T08:30:00",
+            "2022-01-25T09:00:00",
+            "2022-01-25T09:30:00",
+            "2022-01-26T08:00:00",
+            "2022-01-26T08:30:00",
+            "2022-01-26T09:00:00",
+            "2022-01-26T09:30:00"
+    ).collect(Collectors.toList());
+
+    List<String> list3 = Stream.of(
+            "2022-01-01T00:00",
+            "2022-02-01T00:00",
+            "2022-03-01T00:00",
+            "2022-04-01T00:00",
+            "2022-05-01T00:00",
+            "2022-06-01T00:00",
+            "2022-07-01T00:00",
+            "2022-08-01T00:00",
+            "2022-09-01T00:00",
+            "2022-10-01T00:00"
+    ).collect(Collectors.toList());
+
+    List<String> list4 = Stream.of(
+            "2022-01-01T00:00",
+            "2022-01-01T00:00:10",
+            "2022-01-01T00:00:20",
+            "2022-01-01T00:00:30",
+            "2022-01-01T00:00:40",
+            "2022-01-01T00:00:50",
+            "2022-01-01T00:01:00",
+            "2022-01-01T00:01:10",
+            "2022-01-01T00:01:20",
+            "2022-01-01T00:01:30"
+    ).collect(Collectors.toList());
+
+    assertAll(
+            () -> assertEquals("0 * * 24 1 MON", converter.convert(list1)),
+            () -> assertEquals("0 */30 8,9 * 1 *", converter.convert(list2)),
+            () -> assertEquals("0 0 0 1 * *", converter.convert(list3)),
+            () -> assertEquals("*/10 * 0 1 1 SAT", converter.convert(list4))
+    );
+  }
+
+  @Test
+  void convert_shouldThrowException_whenCronExpressionNotFound() {
+    List<String> list = Stream.of(
+            "2022-01-24T19:53:00",
+            "2022-02-24T19:55:00",
+            "2022-03-24T19:54:00",
+            "2022-04-24T20:02:00",
+            "2022-01-24T19:56:00",
+            "2022-01-24T10:57:00",
+            "2022-01-24T19:58:00",
+            "2022-01-24T13:59:00",
+            "2022-01-24T20:00:00",
+            "2022-01-24T21:01:00"
+    ).collect(Collectors.toList());
+    assertThrows(DatesToCronConvertException.class, () -> converter.convert(list));
   }
 
   @Test
@@ -61,5 +120,26 @@ class DatesToCronConverterImplTest {
             () -> assertThrows(DatesToCronConvertException.class, () -> converter.convert(Collections.singletonList("2022-01-24T"))),
             () -> assertThrows(DatesToCronConvertException.class, () -> converter.convert(Collections.singletonList("01-24T20:02:00")))
     );
+  }
+
+  @Test
+  void convert_shouldConvert_whenInputIsOneDate() {
+    assertAll(
+            () -> assertEquals("0 55 19 3 1 MON", converter.convert(Collections.singletonList("2022-01-03T19:55:00"))),
+            () -> assertEquals("0 55 19 4 1 TUE", converter.convert(Collections.singletonList("2022-01-04T19:55:00"))),
+            () -> assertEquals("0 55 19 5 1 WED", converter.convert(Collections.singletonList("2022-01-05T19:55:00"))),
+            () -> assertEquals("0 55 19 6 1 THU", converter.convert(Collections.singletonList("2022-01-06T19:55:00"))),
+            () -> assertEquals("0 55 19 7 1 FRI", converter.convert(Collections.singletonList("2022-01-07T19:55:00"))),
+            () -> assertEquals("0 55 19 8 1 SAT", converter.convert(Collections.singletonList("2022-01-08T19:55:00"))),
+            () -> assertEquals("0 55 19 9 1 SUN", converter.convert(Collections.singletonList("2022-01-09T19:55:00")))
+    );
+  }
+
+  @Test
+  void getImplementationInfo_shouldGetInfo() {
+    assertEquals("Credentials: Nemtsov Dmitriy Sergeevich\n" +
+            "Class: DatesToCronConverterImpl\n" +
+            "package com.nemtsov.dmitriy\n" +
+            "GitHub: https://github.com/dsnemtsov/cron-converter", converter.getImplementationInfo());
   }
 }
